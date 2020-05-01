72fcd6c05e3a45765109b845bc1aef99b5da750c
Adam Greloch <zplhatesbananas@gmail.com>
2020-05-01 16:47:19 +0200

    created OSX-specific config options and custom settings file

--- init.el
+++ init.el
@@ -1,1 +1,15 @@
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("76c5b2592c62f6b48923c00f97f74bcb7ddb741618283bdb2be35f3c0e1030e3" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fringe ((t (:background "nil" :foreground "#DCDCCC")))))
